321e5ee033059dfbb5ebf026f0f2f6ec53c0dc93
byunn90 <kayhanturk90@Live.com>
2023-08-22 15:48:04 +1000

    working on the buttons

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF, faGithub } from "@fortawesome/free-brands-svg-icons";
 import {
   faHome,
   faCar,
@@ -10,14 +11,21 @@ import {
 function NavBar() {
   // Define the states you need
   const [hovered, setHovered] = useState(false);
-  // Home icon Hover
   const [showHomePopup, setShowHomePopup] = useState(false);
+  // For the Home useState Icon
   const [showCarPopUp, setCarPopup] = useState(false);
-  // For the Vechicles icon
   const [showHoveredVechicles, setHoveredVechicles] = useState(false);
-  console.log("showHomePopup:", showHomePopup);
+  // For the Vechicle usestate Icon
+  const [showAboutPopup, setAboutPopup] = useState(false);
+  const [showHoveredAbout, setHoveredAboutPopup] = useState(false);
+  // For the About useState Icon
+  const [showContactPopup, setContactPopup] = useState(false);
+  const [ShowHoveredContact, setHoveredContactPopup] = useState(false);
   return (
-    <nav className="navBar flex justify-between p-4">
+    <nav
+      className="navBar flex justify-between p-4"
+      style={{ backgroundColor: "" }}
+    >
       <div className="pr-10 logo text-1xl font-bold">Logo</div>
       <ul className="flex space-x-4 gap-2">
         <li>
@@ -45,7 +53,7 @@ function NavBar() {
         </li>
         <li>
           <button
-            onClick={() => setCarPopup(!showCarPopUp)}
+            onClick={() => setAboutPopup(!showAboutPopup)}
             className="relative flex items-center justify-center text-white border-b-2 bg-gradient-to-r from-indigo-400 transition ease-in-out delay-350 hover:via-purple-600 to-pink-500 px-2 py-1 rounded font-mono focus:outline-none"
             onMouseEnter={() => setHoveredVechicles(true)}
             onMouseLeave={() => setHoveredVechicles(false)}
@@ -63,25 +71,50 @@ function NavBar() {
             >
               Vehicles
             </span>
+            {/* {New button has to be done here} */}
           </button>
         </li>
         <li>
-          <a
-            href="https://www.w3schools.com"
-            className="text-black md:hover:bg-transparent hover:text-blue-600 font-mono"
+          <button
+            onClick={() => showContactPopup(!setContactPopup)}
+            className=" text-sm relative flex items-center justify-center text-white border-b-2 bg-gradient-to-r from-indigo-400 transition ease-in-out delay-350 hover:via-purple-600 to-pink-500 px-2 py-1 rounded font-mono focus:outline-none"
+            onMouseEnter={() => setHoveredAboutPopup(false)}
+            onMouseLeave={() => setHoveredAboutPopup(true)}
           >
-            <FontAwesomeIcon icon={faInfoCircle} />
+            <FontAwesomeIcon
+              icon={faInfoCircle}
+              className={`absolute transition-opacity duration-1000 ${
+                showHoveredAbout ? "opacity-0" : "opacity-100"
+              }`}
+            ></FontAwesomeIcon>
             About
-          </a>
+            <span
+              className={`text-sm transition-opacity duration-1000 w-15 text-center ${
+                showHoveredAbout ? "opacity-0" : "opacity-100"
+              }`}
+            ></span>
+          </button>
         </li>
         <li>
-          <a
-            href="https://www.w3schools.com"
-            className="block text-black md:hover:bg-transparent hover:text-blue-600"
+          <button
+            onClick={() => showContactPopup(!setContactPopup)}
+            className="text-sm relative flex items-center justify-center text-white border-b-2 bg-gradient-to-r from-indigo-400 transition ease-in-out delay-350 hover:via-purple-600 to-pink-500 px-2 py-1 rounded font-mono focus:outline-none"
+            onMouseLeave={() => setHoveredContactPopup(true)}
+            onMouseEnter={() => setHoveredContactPopup(false)}
           >
-            <FontAwesomeIcon icon={faEnvelope} className="" />
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              className={`absolute transition-opacity duration-1000 ${
+                ShowHoveredContact ? "opacity-0" : "opacity-100"
+              }`}
+            ></FontAwesomeIcon>
+            <span
+              className={`text-sm transition-opacity duration-1000 w-15 text-center ${
+                ShowHoveredContact ? "opacity-0" : "opacity-100"
+              }`}
+            ></span>
             Contact
-          </a>
+          </button>
         </li>
       </ul>
     </nav>
